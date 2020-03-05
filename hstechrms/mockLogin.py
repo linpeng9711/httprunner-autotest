f948233078a3460e8c24a26e442eac6d018c8188
wanglinpeng <lp9711@foxmail.com>
2020-03-05 16:20:05 +0800

    修改python获取配置文件属性

--- hstechrms/mockLogin.py
+++ hstechrms/mockLogin.py
@@ -1,19 +1,21 @@
 # https://segmentfault.com/a/1190000015642808
 import time 
+import os
 from selenium import webdriver # 从selenium导入webdriver
 
 #driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 
 def getCookie(driver):
-    driver.get('http://172.16.100.115:8080/hstechrms/login')
+    base_path = os.environ["base_path"]
+    driver.get(base_path+'/hstechrms/login')
 
     username = driver.find_element_by_id('username$text') #用户名
     password = driver.find_element_by_id('password$text') #密码
     submit = driver.find_element_by_id('login') #登录按钮
     
     #username.send_keys("ywczy03") #输入框输入
-    username.send_keys("jsms_hskj") #输入框输入
-    password.send_keys("orm123456@") #输入框输入
+    username.send_keys(os.environ["login_username"]) #输入框输入
+    password.send_keys(os.environ["login_password"]) #输入框输入
     #token = driver.find_element_by_xpath('//form[@id="loginForm"]/input').get_attribute('value')
     submit.click() #登录
 
